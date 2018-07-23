49a682ebc2aa914d8166c3fecc02874d2f2f0825
Tobide <helitobide@gmail.com>
2018-07-23 21:25:44 +0200

    Codebase du webhook

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//Webhook
+const express = require('express');
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get('/webhook', (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+
+  function envoyerMessageFollowup(agent) {
+    agent.add(`MESSAGE WEBHOOK : Vous avez envoyé "${req.body.queryResult.parameters.message}"`);
+  }
+  const intentMap = new Map();
+  intentMap.set('envoyer.message - demander message', envoyerMessageFollowup);
+
+  agent.handleRequest(intentMap);
+});
+
+
+app.listen(PORT, () => console.log(`Ecoute sur le port ${PORT}.`));
