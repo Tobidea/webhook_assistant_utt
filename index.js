6dd8aadccbcda0fab5f48258b3547e4f3ab0bd9c
Tobide <helitobide@gmail.com>
2018-10-05 11:57:56 +0200

    Updated for new API

--- index.js
+++ index.js
@@ -14,23 +14,35 @@ app.post('/webhook', (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
 
   // Fonction qui gère l'envoi des messages aux développeurs
-  function envoyerMessageFollowup(agent) {
-    console.log(`${agent.intent} : "${agent.parameters.message}"`);
-    agent.add(`MESSAGE WEBHOOK : Vous avez envoyé "${agent.parameters.message}"`);
-  }
+  // async function envoyerMessageFollowup(agent) {
+  //   console.log(`${agent.intent} : "${agent.parameters.message}"`);
+  //
+  //   try{
+  //     return await fetch('http://assistantutt.ga:8080/add/userFeedback', {
+  //       method: 'POST',
+  //       body: {
+  //         contenu: agent.parameters.message,
+  //       },
+  //     })
+  //     .then(() => agent.add(`Vous avez envoyé "${agent.parameters.message}" aux développeurs !`));
+  //   } catch (err) {
+  //     console.log(err);
+  //     agent.add('Une erreur est survenue lors de l\'envoi du message...');
+  //   }
+  // }
 
   async function getInfoUE(agent) {
     if (agent.parameters.codeUE) {
+      // Ce cas de figure s'active lorsque l'utilisateur a bien renseigné une entité @CodeUE
       console.log(`${agent.intent}`);
       console.log(`Recherche d'informations sur l'UE ${agent.parameters.codeUE}`);
 
       try{
-        return await fetch(`http://assistantutt.ga:8080/get/UE?code=${agent.parameters.codeUE}`)
+        return await fetch(`http://assistantutt.ga:8080/ue/${agent.parameters.codeUE}`)
         .then(resultat => resultat.json())
         .then((UE) => {
           if (!UE.error){
-            agent.add(`${UE.code} : ${UE.nom}`);
-            agent.add(UE.description);
+            agent.add(`${UE.code} : ${UE.titre}`);
           } else {
             agent.add(`Désolé, ${agent.parameters.codeUE} n'a pas pu être trouvée...`)
           }
@@ -41,6 +53,7 @@ app.post('/webhook', (req, res) => {
         agent.add('Désolé une erreur est survenue...');
       }
     } else {
+      // Ce cas de figure s'active lorsque l'utilisateur n'a pas renseigné dans sa requête le nom de l'UE recherchée
       agent.add('Sur quelle UE voulez vous des informations?');
     }
 
