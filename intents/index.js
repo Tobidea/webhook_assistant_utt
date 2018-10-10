f8879191c8525f71d957f131e9072a209e62280d
Tobide <helitobide@gmail.com>
2018-10-10 13:29:49 +0200

    Testing webhookClient methods

--- intents/index.js
+++ intents/index.js
@@ -3,10 +3,11 @@ const intents = {}
 intents.handleAboutUE = require('./about-ue');
 
 function test(agent) {
-  console.log(agent.session);
-  console.log(agent.consoleMessages);
-  console.log(agent.query);
-  console.log(agent.contexts);
+  console.log('session: ' +agent.session);
+  console.log('console message: ' + agent.consoleMessages);
+  console.log('alternative : ' + agent.alternativeQueryResults)
+  console.log('query : ' + agent.query);
+  console.log('context : ' + agent.contexts);
 }
 
 intentArrayMapping = [
