6dd8aec0cd15c469206067849cec84de8451d082
Tobide <helitobide@gmail.com>
2018-10-11 10:26:38 +0200

    testing contexts

--- intents/about-ue.js
+++ intents/about-ue.js
@@ -41,7 +41,7 @@ module.exports = async function handleAboutUE(agent) {
       agent.setContext({
         name: 'context-wrongUE',
         lifespan: '2',
-        parameters: {};
+        parameters: {},
       });
     } else {
       agent.add('Pardon, je ne connais pas cette UE !');
