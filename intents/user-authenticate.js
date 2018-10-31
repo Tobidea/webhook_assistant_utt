b64c67681efa8d6c21d385e11d10ad837d182af6
Tobide <helitobide@gmail.com>
2018-10-31 15:05:11 +0100

    Testing setFollowUpEvent

--- intents/user-authenticate.js
+++ intents/user-authenticate.js
@@ -5,7 +5,7 @@ module.exports = async function handleUserAuthenticate(agent) {
 
     const senderId = createSenderId(agent);
 
-    agent.setFollowupEvent('error_USER_NOT_AUTHENTICATED');
     agent.add(`Pour vous authentifier, cliquez sur le lien suivant :`);
     agent.add(`http://assistantutt:8080/api/auth?sender_id=${senderId}`);
+    agent.setFollowupEvent('error_USER_NOT_AUTHENTICATED');
 }
