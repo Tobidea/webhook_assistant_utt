e63115118f6e831b0bdb5fae3d86120a74f5b7c3
Tobide <helitobide@gmail.com>
2018-10-31 16:31:58 +0100

    Fixing fetching problems

--- helpers/fetchPrivateUserInfo.js
+++ helpers/fetchPrivateUserInfo.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 module.exports = async function fetchPrivateUserInfo(senderId) {
 
-    const result = fetch('http://assistantutt.ga:8080/api/users/private/account', {
+    const result = await fetch('http://assistantutt.ga:8080/api/users/private/account', {
         method: 'GET',
         headers: {
             'Sender-Id': senderId,
