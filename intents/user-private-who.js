956e3d5c3d2c8ed84312acd668d0e0704abb9af9
Tobide <helitobide@gmail.com>
2018-10-31 16:29:12 +0100

    Fixed url for user-authenticate

--- intents/user-private-who.js
+++ intents/user-private-who.js
@@ -9,7 +9,7 @@ module.exports = async function handleUserPrivateWho(agent) {
     try {
         const result = await fetchPrivateUserInfo(senderId);
         
-        if (response.error) {
+        if (result.error) {
             return agent.setFollowupEvent('error_USER_NOT_AUTHENTICATED');
         } else {
             return agent.add(`Tu es ${result.firstname} non?`);
