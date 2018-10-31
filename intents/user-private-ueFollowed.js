358b074951feb0021b7b90934d7b38db5beabc73
Tobide <helitobide@gmail.com>
2018-10-31 18:26:47 +0100

    Debugging

--- intents/user-private-ueFollowed.js
+++ intents/user-private-ueFollowed.js
@@ -1,10 +1,11 @@
 const isAlreadyInContext = require('../helpers/isAlreadyInContext');
 const fetchPrivateUserInfo = require('../helpers/fetchPrivateUserInfo');
 
-module.exports = async function handleUserPrivateUeFollowed() {
-    console.log(`${agent.intent} called with parameters : ${JSON.stringify(agent.parameters)}`)
-
-    const userInfo = await isAlreadyInContext({
+module.exports = async function handleUserPrivateUeFollowed(agent) {
+    try {
+        console.log(`${agent.intent} called with parameters : ${JSON.stringify(agent.parameters)}`)
+        
+        const userInfo = await isAlreadyInContext({
         agent,
         contextName: 'privateUserInfo',
         callback: async () => {
@@ -13,12 +14,16 @@ module.exports = async function handleUserPrivateUeFollowed() {
     });
 
     console.log(userInfo);
-
+    
     // To create the list of UEs
     const ueString = userInfo.uvs.reduce((accumulator, currentUv) => (
         accumulator + ', ' + currentUv
-    ));
-
-    return agent.add(`Vos UE ce semestre sont ${ueString}.`);
+        ));
+        
+        return agent.add(`Vos UE ce semestre sont ${ueString}.`);
+    } catch (err) {
+        console.log(err);
+        return agent.add(`J'ai eu un soucis en essayant de savoir vos UE !`);
+    }
     
 }
