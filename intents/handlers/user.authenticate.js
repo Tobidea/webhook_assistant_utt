2a9ecfa12e15930855f1ad8210b7f51a277ca708
Tobide <helitobide@gmail.com>
2018-12-16 15:31:34 +0100

    Added custom payload for facebook authentication button (Fix 4)

--- intents/handlers/user.authenticate.js
+++ intents/handlers/user.authenticate.js
@@ -3,30 +3,30 @@ const { Payload } = require('dialogflow-fulfillment');
 module.exports = async function handleUserAuthenticate(agent) {
     console.log(`${agent.intent} called with parameters : ${JSON.stringify(agent.parameters)}`)
 
-    const fbPayload = {
+    const fbPayloadOptions = {
         "attachment":{
             "type":"template",
             "payload":{
             "template_type":"button",
-            "text":"Try the URL button!",
+            "text":"Appuie sur le bouton pour t'authentifier.",
             "buttons":[
                 {
                 "type":"web_url",
                 "url":`http://assistantutt.ga:8080/api/auth?sender_id=${agent.senderId}`,
-                "title":"Authentification",
-                "webview_height_ratio": "full"
+                "title":"Authentification avec son compte UTT",
+                "webview_height_ratio": "tall"
                 }
             ]
             }
         }
     }
 
-    const button = new Payload(agent.FACEBOOK, fbPayload, {
+    const fbButton = new Payload(agent.FACEBOOK, fbPayloadOptions, {
         sendAsMessage: true,
     });
 
-    agent.add(`Pour vous authentifier, cliquez sur le lien suivant :`);
+    agent.add(`Pour t'authentifier, allez sur le lien suivant :`);
     agent.add(`http://assistantutt.ga:8080/api/auth?sender_id=${agent.senderId}`);
-    agent.add(button);
+    agent.add(fbButton);
 
 }
