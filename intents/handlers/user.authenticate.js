c896a45a510d5541b2306eb50447c65af23f7887
Tobide <helitobide@gmail.com>
2018-12-16 15:23:52 +0100

    Added custom payload for facebook authentication button (Fix 3)

--- intents/handlers/user.authenticate.js
+++ intents/handlers/user.authenticate.js
@@ -4,26 +4,26 @@ module.exports = async function handleUserAuthenticate(agent) {
     console.log(`${agent.intent} called with parameters : ${JSON.stringify(agent.parameters)}`)
 
     const fbPayload = {
-        "message":{
-            "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"button",
-                "text":"Try the URL button!",
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":`http://assistantutt.ga:8080/api/auth?sender_id=${agent.senderId}`,
-                    "title":"Authentification",
-                    "webview_height_ratio": "full"
-                  }
-                ]
-              }
+        "attachment":{
+            "type":"template",
+            "payload":{
+            "template_type":"button",
+            "text":"Try the URL button!",
+            "buttons":[
+                {
+                "type":"web_url",
+                "url":`http://assistantutt.ga:8080/api/auth?sender_id=${agent.senderId}`,
+                "title":"Authentification",
+                "webview_height_ratio": "full"
+                }
+            ]
             }
         }
     }
 
-    const button = new Payload(agent.FACEBOOK, fbPayload);
+    const button = new Payload(agent.FACEBOOK, fbPayload, {
+        sendAsMessage: true,
+    });
 
     agent.add(`Pour vous authentifier, cliquez sur le lien suivant :`);
     agent.add(`http://assistantutt.ga:8080/api/auth?sender_id=${agent.senderId}`);
